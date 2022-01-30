d76d402e6dd3848b5ec30c876efb913cd7e8a63b
april <april@aprildeMacBook-Air.local>
2022-01-30 17:13:36 +0800

    feat: 配置css module

--- src/layout/Editor/index.tsx
+++ src/layout/Editor/index.tsx
@@ -1,17 +1,13 @@
 import { Button } from "antd";
 import Draggable from "react-draggable";
-import './index.css'
-
-
+import style from './index.module.scss';
 
 function Editor() {
   return (
-    <div className='container'>
-        <div className='toolbar' style={{height: '32px', }} > 
-            
-        </div>
-        <div className='draggable-container'>
-          <div className='editing-slide'>
+    <div className={style.container} >
+        <div className='toolbar' style={{height: '32px', }} ></div>
+        <div className={style.draggableContainer}>
+          <div className={style.editingSlide}>
           <Draggable allowAnyClick bounds='parent' onStop={(e: any)=>console.log(e.path[1].style.transform)} >
               <Button size='middle' style={{height:'32px'}}>文本</Button>
             </Draggable>
